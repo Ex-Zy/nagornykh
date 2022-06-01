484522c8bc880776bf5ac6f58ff0026435afe59e
Sergey Nagornykh <serega1905@ukr.net>
2022-06-01 17:59:43 +0300

    Refactor main.js

--- public/main.js
+++ public/main.js
@@ -1,21 +1,22 @@
 "use strict";
 
-const isMobileUserAgent = () => {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  );
-};
-
 class Screen {
   constructor({ width, height }) {
     this.screen = { width, height };
   }
+
+  isMobileUserAgent = () => {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+  };
 }
 
-class BrowserScreen extends Screen {
+class ScreenMouseTracker extends Screen {
   constructor({ width, height }) {
     super({ width, height });
     this.mouse = { x: 0, y: 0 };
+    this.mouseStored = { ...this.mouse };
   }
 
   setMouseCoords = (event) => {
@@ -28,100 +29,105 @@ class BrowserScreen extends Screen {
   }
 }
 
+class ScreenWithMouseStored extends ScreenMouseTracker {
+  constructor({ width, height }) {
+    super({ width, height });
+    this.mouseStored = { ...this.mouse };
+  }
+
+  updateMouseStored({ x, y }) {
+    this.mouseStored = { x, y };
+  }
+
+  isStoredMousePosition = () => {
+    return (
+      this.mouseStored.x === this.mouse.x && this.mouseStored.y === this.mouse.y
+    );
+  };
+}
+
 class Svg {
   constructor(svg) {
     this.svg = svg;
   }
 
-  setupSvgViewBox = ({ width, height }) => {
-    this.svg.setAttribute("viewBox", `0 0 ${width} ${height}`);
+  remove = () => {
+    this.svg.remove();
   };
 }
 
-class Circle extends Svg {
+class SvgCircle extends Svg {
   constructor(svg) {
     super(svg);
     this.circle = svg.querySelector("circle");
   }
 
+  setupSvgViewBox = ({ width, height }) => {
+    this.svg.setAttribute("viewBox", `0 0 ${width} ${height}`);
+  };
+
   setupCircleTranformOrigin = (transformOrigin) => {
     gsap.set(this.circle, { transformOrigin });
   };
+
+  setupSvg = ({ width, height, transformOrigin }) => {
+    this.setupSvgViewBox({ width, height });
+    this.setupCircleTranformOrigin(transformOrigin);
+  };
 }
 
-class CircleAnimation {
-  constructor({ screen, circle }) {
-    this.screen = screen;
-    this.circle = circle;
-    this.mouseStored = { ...this.screen.mouse };
+class Animation {
+  constructor({ screenInstance, svgCircleInstance }) {
+    this.screenInstance = screenInstance;
+    this.svgCircleInstance = svgCircleInstance;
 
     this.init();
   }
 
   init = () => {
-    // Stop using animation for mobile
-    if (isMobileUserAgent()) {
-      return this.removeSvg();
+    // Remove svg for mobile
+    if (this.screenInstance.isMobileUserAgent()) {
+      return this.svgCircleInstance.remove();
     }
-
-    const { screen } = this.screen;
-    const { width, height } = screen;
-
-    this.setupSvg({ width, height, transformOrigin: "50% 50%" });
-    this.trackMousePosition();
+    this.baseSetup();
     this.runAnimation();
   };
 
+  baseSetup = () => {
+    const { width, height } = this.screenInstance.screen;
+    const svgConfig = { width, height, transformOrigin: "50% 50%" };
+
+    this.svgCircleInstance.setupSvg({ ...svgConfig });
+    this.screenInstance.trackMousePosition();
+  }
+
   runAnimation = () => {
-    gsap.ticker.add(this.animateCircle);
-  };
+    gsap.ticker.add(() => {
+      // stop animation
+      if (this.screenInstance.isStoredMousePosition()) return;
 
-  animateCircle = () => {
-    if (this.isStoredMousePosition()) return;
+      const { circle } = this.svgCircleInstance;
+      const { mouse } = this.screenInstance;
+      const animationConfig = {
+        x: mouse.x,
+        y: mouse.y,
+        ease: Elastic.easeOut.config(1.25, 1),
+        duration: 0.5,
+        delay: 0.1,
+        opacity: 1,
+      };
 
-    const { circle } = this.circle;
-    const { mouse } = this.screen;
-    const animationConfig = {
-      ...CircleAnimation.baseAnimationConfig,
-      ...mouse,
-    };
-
-    gsap.to(circle, animationConfig);
-    this.mouseStored = { ...mouse };
-  };
-
-  setupSvg = ({ width, height, transformOrigin }) => {
-    this.circle.setupSvgViewBox({ width, height });
-    this.circle.setupCircleTranformOrigin(transformOrigin);
-  };
-
-  removeSvg = () => {
-    this.circle.svg.remove();
-  };
-
-  trackMousePosition = () => {
-    this.screen.trackMousePosition();
-  };
-
-  isStoredMousePosition = () => {
-    return (
-      this.mouseStored.x === this.screen.mouse.x &&
-      this.mouseStored.y === this.screen.mouse.y
-    );
+      gsap.to(circle, animationConfig);
+      this.screenInstance.updateMouseStored({ ...mouse });
+    });
   };
 }
 
-CircleAnimation.baseAnimationConfig = {
-  ease: Elastic.easeOut.config(1.25, 1),
-  duration: 0.5,
-  delay: 0.1,
-  opacity: 1,
-};
-
-new CircleAnimation({
-  screen: new BrowserScreen({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  }),
-  circle: new Circle(document.querySelector("svg")),
+const screenInstance = new ScreenWithMouseStored({
+  width: window.innerWidth,
+  height: window.innerHeight,
 });
+
+const svgCircleInstance = new SvgCircle(document.querySelector("svg"));
+
+new Animation({ screenInstance, svgCircleInstance });
