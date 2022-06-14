4386fe077a0a9ef93ae5d56c55957d90e7da019f
Sergey Nagornykh <serega1905@ukr.net>
2022-06-14 23:09:05 +0300

    refactor js

--- src/javascript/modules/circle/animation.js
+++ src/javascript/modules/circle/animation.js
@@ -1,34 +1,34 @@
 import Canvas from "./canvas";
 import { gsap, Elastic } from "gsap";
 
+const config = {
+  ease: Elastic.easeOut.config(1.25, 1),
+  duration: 0.5,
+  delay: 0.1,
+  opacity: "var(--circle-alpha)",
+};
+
 class Animation extends Canvas {
   constructor({ screenInstance, svgCircleInstance }) {
     super({ screenInstance, svgCircleInstance });
   }
 
-  handler = () => {
+  #screenHandler = () => {
     if (this.screenInstance.isStoredMousePosition()) return;
 
     const { circle } = this.svgCircleInstance;
     const { mouse } = this.screenInstance;
 
-    gsap.to(circle, {
-      x: mouse.x,
-      y: mouse.y,
-      ease: Elastic.easeOut.config(1.25, 1),
-      duration: 0.5,
-      delay: 0.1,
-      opacity: 'var(--circle-alpha)'
-    });
+    gsap.to(circle, { x: mouse.x, y: mouse.y, ...config });
     this.screenInstance.updateMouseStored({ ...mouse });
   };
 
   runAnimation = () => {
-    gsap.ticker.add(this.handler);
+    gsap.ticker.add(this.#screenHandler);
   };
 
   stopAnimation = () => {
-    gsap.ticker.remove(this.handler);
+    gsap.ticker.remove(this.#screenHandler);
   };
 }
 
--- src/javascript/modules/circle/index.js
+++ src/javascript/modules/circle/index.js
@@ -2,10 +2,12 @@ import { ScreenInstance } from "./screen";
 import { SvgCircleInstance } from "./svg";
 import Animation from "./animation";
 
+// create animation instance
 const screenInstance = new ScreenInstance({width: window.innerWidth, height: window.innerHeight});
 const svgCircleInstance = new SvgCircleInstance(document.querySelector("svg"));
 const animationInstance = new Animation({ screenInstance, svgCircleInstance });
 
+// public api
 const circle = {
   runAnimation: animationInstance.runAnimation,
   stopAnimation: animationInstance.stopAnimation,
