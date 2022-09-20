8589171bf8831f77b4de628360cf1d07864d17e3
Sergey Nagornykh <serega1905@ukr.net>
2022-09-20 12:36:39 +0300

    minor refactor

--- src/javascript/modules/circle/index.js
+++ src/javascript/modules/circle/index.js
@@ -1,16 +1,14 @@
 import { Animation, Svg, Screen } from "./setup";
 
-const rawSvgTemplate = `<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" aria-hidden="true" viewBox="0 0 1600 900"><circle class="circle" r="80" cx="0" cy="0" /></svg>`;
-
-// create animation instance
-const screenInstance = new Screen({width: window.innerWidth, height: window.innerHeight });
-const svgCircleInstance = new Svg(rawSvgTemplate);
-const animationInstance = new Animation({ screenInstance, svgCircleInstance });
+const circleAnimation = new Animation({
+  screenInstance: new Screen({width: window.innerWidth, height: window.innerHeight }),
+  svgCircleInstance: new Svg(`<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" aria-hidden="true" viewBox="0 0 1600 900"><circle class="circle" r="80" cx="0" cy="0" /></svg>`),
+});
 
 // public api
 const circle = {
-  runAnimation: animationInstance.runAnimation,
-  stopAnimation: animationInstance.stopAnimation,
+  runAnimation: circleAnimation.runAnimation,
+  stopAnimation: circleAnimation.stopAnimation,
 };
 
 export default circle;
